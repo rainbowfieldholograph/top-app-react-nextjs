90dc88d920976ed401d76ad4587c357ef18b51c7
rainbowfieldholograph <somebleed@tutanota.com>
2022-01-25 16:58:56 +0900

    added sort accessability

--- components/sort/Sort.tsx
+++ components/sort/Sort.tsx
@@ -2,22 +2,24 @@ import { SortEnum, SortProps } from './Sort.props'
 import styles from './Sort.module.css'
 import { SvgIcon } from '../svgIcon/SvgIcon'
 import { IconTypes } from '../svgIcon/SvgIcon.props'
+import { KeyboardEvent } from 'react'
 
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
   return (
     <div className={[styles.sort, className].join(' ')} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
-        className={[sort === SortEnum.Rating ? styles.active : ''].join(' ')}
+        className={sort === SortEnum.Rating ? styles.active : ''}
       >
         <SvgIcon className={styles.sortIcon} iconType={IconTypes.sort} /> По рейтингу
-      </span>
-      <span
+      </button>
+      <button
+        tabIndex={0}
         onClick={() => setSort(SortEnum.Price)}
-        className={[sort === SortEnum.Price ? styles.active : ''].join(' ')}
+        className={sort === SortEnum.Price ? styles.active : ''}
       >
         <SvgIcon className={styles.sortIcon} iconType={IconTypes.sort} /> По цене
-      </span>
+      </button>
     </div>
   )
 }
