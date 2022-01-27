432c553c1b115b12375770416dac2749ca25c458
rainbowfieldholograph <somebleed@tutanota.com>
2022-01-27 17:35:58 +0900

    deploy preparing

--- components/Ptag/Ptag.props.ts
+++ components/Ptag/Ptag.props.ts
@@ -1,7 +1,6 @@
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
+import { DetailedHTMLProps, HTMLAttributes } from 'react'
 
 export interface PtagProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
   size?: 'small' | 'medium' | 'large'
-  children?: ReactNode
 }
